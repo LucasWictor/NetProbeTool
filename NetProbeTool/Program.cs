eab435004a7b6557ca7bf32eb8a83d5492330a85
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-08 20:06:23 +0200

    Added TcpService

--- NetProbeTool/Program.cs
+++ NetProbeTool/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Infrastructure.Services;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Example of checking command-line arguments
+        if (args.Length > 0 && args[0] == "--start-server")
+        {
+            int port = 8888; // Default port
+            var tcpService = new TcpService(port);
+            await tcpService.StartListeningAsync();
+        }
+    }
+}
