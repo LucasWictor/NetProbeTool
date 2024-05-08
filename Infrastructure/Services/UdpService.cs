d8f7a1aee997883770c8589ef3f630e5e4772877
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-08 20:22:33 +0200

    Added UdpService

--- Infrastructure/Services/UdpService.cs
+++ Infrastructure/Services/UdpService.cs
@@ -1,12 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Services
 {
-    internal class UdpService
+    public class UdpService
     {
+        private UdpClient UdpClient;
+        private int ListenPort;
+        
+        public UdpService(int port)
+        {
+            ListenPort = port;
+            UdpClient = new UdpClient(ListenPort);
+        }
+        public void StartListening()
+        {
+            Task.Run(async () => await ReciveMessageAsync());
+        }
+
+        public async Task ReciveMessageAsync()
+        {
+            try
+            {
+                while (true)
+                {
+                    var result = await UdpClient.ReceiveAsync();
+                    string RecivedData = Encoding.ASCII.GetString(result.Buffer);
+                    Console.WriteLine($"Recived: {RecivedData} from {result.RemoteEndPoint}");
+
+                    //Echo the message back to the sender 
+                    await SendMessageAsync(RecivedData, result.RemoteEndPoint);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error receiving UDP data: {ex.Message}");
+            }
+        }
+
+        public async Task SendMessageAsync(string message, IPEndPoint endpoint)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            await UdpClient.SendAsync(buffer, buffer.Length,  endpoint);
+        }
     }
 }
