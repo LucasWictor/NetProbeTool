a41c4859eb309815a680c1b89bbea18d76cdb185
LucasWictor <126620359+LucasWictor@users.noreply.github.com>
2024-05-09 11:45:18 +0200

    Updated PortScanner

--- Infrastructure/Scanners/PortScanner.cs
+++ Infrastructure/Scanners/PortScanner.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
-namespace Infrastructure.Scanners
-{
-    internal class PortScanner
+    namespace Infrastructure.Services
     {
+    public class PortScanner
+    {
+        public async Task ScanPorts(string host, int startPort, int endPort)
+        {
+            var results = new List<(int Port, string Status)>();
+
+            var tasks = Enumerable.Range(startPort, endPort - startPort + 1).Select(port => Task.Run(async () =>
+            {
+                try
+                {
+                    using (var client = new TcpClient())
+                    {
+                        await client.ConnectAsync(host, port);
+                        return (Port: port, Status: $"Port {port} is open.");
+                    }
+                }
+                catch (SocketException)
+                {
+                    return (Port: port, Status: $"Port {port} is closed.");
+                }
+            })).ToList();
+
+            var scanResults = await Task.WhenAll(tasks);
+
+            // Add results synchronized
+            foreach (var result in scanResults)
+            {
+                results.Add(result);
+            }
+
+            // Sort by port number before printing
+            foreach (var result in results.OrderBy(r => r.Port))
+            {
+                Console.WriteLine(result.Status);
+            }
+        }
     }
 }
